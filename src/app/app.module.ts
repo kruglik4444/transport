e89f6d46718913a31f159bc31be5b7bd09d8ed54
Dima Kruglov <kruglov.dv@phystech.edu>
2023-12-16 01:14:49 +0300

    firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase: {
+        projectId: 'krugwagen',
+        appId: '1:682826172685:web:0f220f8fe115ab14b94844',
+        storageBucket: 'krugwagen.appspot.com',
+        locationId: 'us-central',
+        apiKey: 'AIzaSyBCuaOYS9ztJgUWVni9qcjzNWvSod0AO0I',
+        authDomain: 'krugwagen.firebaseapp.com',
+        messagingSenderId: '682826172685',
+    },
+    production: false
+}
