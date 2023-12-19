6e60ae31581a6833b28ade0b175cfd12d0cf0d11
Dima Kruglov <kruglov.dv@phystech.edu>
2023-12-20 00:24:27 +0300

    login page

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../../models/user.model';
+import { LoginService } from '../../pages/login/login.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  user$!: BehaviorSubject<User | null>;
+
+  constructor(private loginService: LoginService) {}
+
+  ngOnInit(): void {
+    this.user$ = this.loginService.user;
+  }
 
 }
